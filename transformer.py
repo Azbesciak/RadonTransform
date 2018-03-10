262dfc616e04e26c6b84e7a4f2c760013e03bf35
Witold Kupś <witold.kups@interlan.pl>
2018-03-10 10:19:50 +0100

    Added sinogram filtering

--- transformer.py
+++ transformer.py
@@ -1,12 +1,16 @@
 from __future__ import print_function, division
 
-import numpy as np
 import matplotlib.pyplot as plt
-from numpy.fft import fft, ifft
-
-from skimage.io import imread
-from skimage.transform import radon, rescale, rotate, iradon
+import numpy as np
+from numpy.fft import fftfreq, fft, ifft
 from skimage.filters import gaussian
+from skimage.io import imread
+from skimage.transform import rescale, rotate
+
+image_name = "CT_ScoutView.jpg"
+# image_name = "Shepp_logan.jpg"
+# image_name = "kolo.jpg"
+
 
 def draw_image(i, img):
     plt.subplot(1, 2, i)
@@ -45,70 +49,100 @@ def increase_image(img):
     return pad_image(img, int(np_max_dim * np.sqrt(2)), shape)
 
 
+def create_filter_at(target, start, distance, filter):
+    half_len = len(filter) // 2
+    for i in range(len(filter)):
+        to_write = i - half_len,
+        target[:, start+to_write[0]::distance] = filter[i]
+
+
 def prepare_tomograph(emitters, dim):
     emitters = np.min((emitters, dim))
     zeros = np.zeros((dim, dim))
     distance = int(dim / emitters)
     start = int(np.ceil((dim % distance) / 2))
-    zeros[:, start::distance] = 1
+    create_filter_at(zeros, start, distance, [1])
     zeros = gaussian(zeros)
+    zeros /= zeros.max()
     return zeros
 
 
 def get_intersection(rotation, image, tomograph, real_dim):
     rotation += 90
-    start = int((len(image) - real_dim) / 2)
+    target_size = len(image)
+    start = int((target_size - real_dim) / 2)
     rotated = rotate(tomograph, rotation)
     common_part = rotated * image
     common_rotated_again = rotate(common_part, -rotation)
-    column_avg = [sum(q) / real_dim for q in common_rotated_again[start:(start+real_dim)]]
+    column_avg = [sum(q)/real_dim for q in common_rotated_again[start:(start+real_dim)]]
     return column_avg
 
 
-def inverse_radon(image, rotations):
-    output_size = len(image)
-    projection = fft(image, axis=0)
+def make_radon(increased_image, increased_tomograph, real_dim, theta):
+    res = []
+    for rotation in theta:
+        res.append(get_intersection(rotation, increased_image, increased_tomograph, real_dim))
+    res = np.array(res)
+    return res
+
+
+def transform_sinogram(sinogram):
+    sinogram = np.rot90(sinogram, k=1)
+    freqs = sinogram.shape[0]
+    projection_size_padded = \
+        max(64, int(2 ** np.ceil(np.log2(2 * freqs))))
+    pad_width = ((0, projection_size_padded - freqs), (0, 0))
+    img = np.pad(sinogram, pad_width, mode='constant', constant_values=0)
+    f = fftfreq(projection_size_padded).reshape(-1, 1)
+    omega = 2 * np.pi * f
+    fourier_filter = 2 * np.abs(f)# * np.cos(omega)
+    projection = fft(img, axis=0) * fourier_filter
     radon_filtered = np.real(ifft(projection, axis=0))
-    reconstructed = np.zeros((output_size, output_size))
-    mid_index = image.shape[0] // 2
-
-    [X, Y] = np.mgrid[0:output_size, 0:output_size]
-    xpr = X - int(output_size) // 2
-    ypr = Y - int(output_size) // 2
-    th = (np.pi / 180.0) * rotations
-    # Reconstruct image by interpolation
-    for i in range(len(rotations)):
-        theta = th[i]
-        t = ypr * np.cos(theta) - xpr * np.sin(theta)
-        x = np.arange(radon_filtered.shape[0]) - mid_index
-        reconstructed += np.interp(t, x, radon_filtered[:, i],left=0, right=0)
-    return reconstructed
+    radon_filtered = radon_filtered[:freqs, :]
+    return np.rot90(radon_filtered, k=-1)
 
 
-image = imread("examples/CT_ScoutView.jpg", as_grey=True)
+def inverse_radon(sigmoid, rotations, output_size):
+    reconstructed = increase_image(np.zeros((output_size, output_size)))
+    start = (len(reconstructed) - output_size)//2
+    end = start + output_size
+    rotations_len = len(rotations)
+    for i in range(rotations_len):
+        temp = np.array([sigmoid[i], ] * len(reconstructed))
+        temp = make_image_square(temp)
+        reconstructed += rotate(temp, rotations[i])
+    result = reconstructed[start:end, start:end]
+    if rotations_len > 0:
+        result /= rotations_len
+    # result[result<0] = 0
+    # result = adjust_gamma(result, 2)
+    return result
+
+
+image = imread("examples/" + image_name, as_grey=True)
 image = rescale(image, scale=0.4, mode='reflect')
+max_image_value = np.max(image)
+if max_image_value > 1:
+    image /= 255
 image = make_image_square(image)
 increased_image = increase_image(image)
 tomograph = prepare_tomograph(emitters=emitters_num, dim=np.max(image.shape))
 increased_tomograph = increase_image(tomograph)
 
-res = []
-theta = np.linspace(0., 180., max(image.shape), endpoint=False)
-rotations = range(0, 360, 1)
-for rotation in theta:
-    res.append(get_intersection(rotation, increased_image, increased_tomograph, len(image)))
-res = np.array(res)
-fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(8, 8))
-
-sinogram = radon(image, theta=theta, circle=True)
-
-i_sin = inverse_radon(sinogram, theta)
+theta = np.linspace(0., 360., 1440, endpoint=False)
+sinogram = make_radon(increased_image, increased_tomograph, len(image), theta)
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8, 8))
+sinogram_transformed = transform_sinogram(sinogram)
+# theta = np.linspace(0., 180., max(image.shape), endpoint=False)
+# sinogram = radon(image, theta=theta, circle=True)
+i_sin = inverse_radon(sinogram_transformed, theta, len(image))
+# i_sin = iradon(sinogram, theta=theta)
 
 ax1.imshow(image, cmap=plt.cm.Greys_r)
-ax2.imshow(sinogram, cmap=plt.cm.Greys_r,
-           extent=(0, 180, 0, sinogram.shape[0]), aspect='auto')
-ax4.imshow(i_sin, cmap=plt.cm.Greys_r)
-ax3.imshow(res, cmap=plt.cm.Greys_r)
+# ax2.imshow(res, cmap=plt.cm.Greys_r,
+#            extent=(0, 180, 0, res.shape[0]), aspect='auto')
+ax2.imshow(sinogram, cmap=plt.cm.Greys_r)
+ax3.imshow(i_sin, cmap=plt.cm.Greys_r)
 
 fig.tight_layout()
 plt.show()
